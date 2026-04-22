2a65f46302ecf1424f1f9385088e979995986f63
AdrielSRamz <adrielsilva@live.com.ar>
2026-04-21 21:00:23 -0300

    Funcion generate block
    se creo la funion generate block y se la llama desde button down. Ademas se precargo el asset red block y violet block.

--- scenes/Buttons/RedGenerator/red_generator.gd
+++ scenes/Buttons/RedGenerator/red_generator.gd
@@ -1,5 +1,10 @@
 extends TextureButton
-
+@onready var RedBlock = preload("res://assets/block_red.png")
 
 func _on_button_down() -> void:
+	generate_red_block()
 	pass # Replace with function body.
+
+func generate_red_block():
+	var NewRedBlock = RedBlock.instantiate()
+	add_child(NewRedBlock)
--- scenes/Buttons/VioletGenerator/violet_generator.gd
+++ scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -1,5 +1,10 @@
 extends TextureButton
-
+@onready var VioletBlock = preload("res://assets/block_violet.png")
 
 func _on_button_down() -> void:
+	generate_violet_block()
 	pass # Replace with function body.
+
+func generate_violet_block():
+	var NewVioletBlock = VioletBlock.instantiate()
+	add_child(NewVioletBlock)
